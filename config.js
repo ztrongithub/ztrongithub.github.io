27283a93213b1be391fc07e2efe051688106fc6a
ztrongithub <120009261+ztrongithub@users.noreply.github.com>
2023-01-03 21:46:25 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793391640-6d8ca58a0eb407b7ae41ac85',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/zhongtianrui'
+    },
+    {
+      text: 'Blog',
+      url: 'http://ztrztr.ml'
+    },
+  ],
+};
