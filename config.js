fd2417cf87e5b4b1ead5a4daa9f5f6133bc761db
ztrongithub <120009261+ztrongithub@users.noreply.github.com>
2023-01-03 21:57:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm793391640-6d8ca58a0eb407b7ae41ac85', 'm793391713-8e5afb88e6f37510d71341ea'
+    'm793391640-6d8ca58a0eb407b7ae41ac85', 'm793391713-8e5afb88e6f37510d71341ea',
   ],
 
   // 日志天数
